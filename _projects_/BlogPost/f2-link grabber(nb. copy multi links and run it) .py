503a7a0eb3a44c7eb2686829f5b6b3230d5d60e3
kds-zahid <90888332+kds-zahid@users.noreply.github.com>
2023-09-25 07:52:40 +0600

    a

--- _projects_/BlogPost/f2-link grabber(nb. copy multi links and run it) .py
+++ _projects_/BlogPost/f2-link grabber(nb. copy multi links and run it) .py
@@ -11,8 +11,9 @@ tag_name=input("Tag name:")
 with open('linksText.txt', 'w') as file:
     for url in url_array:
         response = requests.get(url)
+        encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')    
+            soup = BeautifulSoup(response.text, 'html.parser', from_encoding=encoding)    
             for tag in soup.find_all(tag_name):
                 print(tag.text)        
                 file.write(tag.text + '\n')
